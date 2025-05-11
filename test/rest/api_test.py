aa22c1feb552fafb651e4359b27c20872339ff2e
Yeray Peñate <yeraypg@gmail.com>
2025-05-11 16:49:58 +0100

    first commit

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -5,7 +5,7 @@ from urllib.request import urlopen
 
 import pytest
 
-BASE_URL = "http://localhost:5000"
+BASE_URL = "http://127.0.0.1:5000"
 BASE_URL_MOCK = "http://localhost:9090"
 DEFAULT_TIMEOUT = 2  # in secs
 
